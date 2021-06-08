8a29fce1205da907eadbb7ca2bba19fe1534db2b
boiledRice <ruqsaq@protonmail.com>
2021-06-08 15:53:49 +0200

    Added new functionality

--- src/com/boiledRice/Main.java
+++ src/com/boiledRice/Main.java
@@ -1,13 +1,22 @@
 package com.boiledRice;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * It's the Main class of the Hello World project created to test functionality of Git and GitHub.
- * @version 0.1
+ * @version 0.2
+ * @since 0.1
  * @author boiledRice
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello, World!");
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+        System.out.println(reader.readLine());
     }
 }
