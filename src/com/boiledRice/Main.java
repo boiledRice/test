08e6bf2c606b167a4126b4de5e23fdc933c02277
boiledRice <ruqsaq@protonmail.com>
2021-06-08 15:40:53 +0200

    First commit

---
+++ src/com/boiledRice/Main.java
@@ -0,0 +1,13 @@
+package com.boiledRice;
+
+/**
+ * It's the Main class of the Hello World project created to test functionality of Git and GitHub.
+ * @version 0.1
+ * @author boiledRice
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
